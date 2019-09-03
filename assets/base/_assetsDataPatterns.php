bdaa4a9814d0de923844f2d92e1cd95220a40a1a
Pedro Plowman <pedrofp@me.com>
2019-09-03 16:12:32 +1000

    Update _assetsDataPatterns.php

--- assets/base/_assetsDataPatterns.php
+++ assets/base/_assetsDataPatterns.php
@@ -12,9 +12,32 @@
  */
 
 return [
+	// Pattern for NPM/Unpkg assets
 	'assetName' => [
 		'version' => '0.0.0',
 		'path' => 'path',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'cssOptions' => [
+			'cssOptions'
+		],
+		'jsOptions' => [
+			'jsOptions'
+		],
+		'publishOptions' => [
+			'publishOptions'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	// Pattern for ../vendor assets
+	'assetName' => [
+		'sourcePath' => '@p2m/yii2-p2y2-things/vendor/...',
 		'css' => [
 			'asset.min.css'
 		],
