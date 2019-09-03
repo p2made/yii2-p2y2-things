b6d5fbaca4f173bfb83290e8a9a9b6339d283e91
Pedro Plowman <pedrofp@me.com>
2019-09-03 14:29:00 +1000

    composer

--- assets/base/_assetsData.php
+++ assets/base/_assetsData.php
@@ -12,4 +12,138 @@
  */
 
 return [
+	'animate.css' => [
+		'version' => '3.7.2',
+		'css' => [
+			'animate.min.css'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'chart.js' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'flag-icon-css' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'moment' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'moment-timezone' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'assetName' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'assetName' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'assetName' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'assetName' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'assetName' => [
+		'version' => '0.0.0',
+		'css' => [
+			'asset.min.css'
+		],
+		'js' => [
+			'asset.min.js'
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	],
+	'assetName' => [
+		// asset contents
+	],
 ];
+
+
+
+
+
+	"": "~2.8",
+	"": "~3.3",
+	"": "~3.4",
+	"": "~2.24",
+	"": "~0.5",
+	"scrollreveal": "~4.0",
+	"simple-line-icons": "~2.4",
+	"sweetalert": "~2.1",
+	"jszip": "~3.2",
