95235fad0c7730f66b2dfd0c8a989aef253ea98a
Pedro Plowman <pedrofp@me.com>
2019-09-03 12:08:25 +1000

    Update _assetsData.php

--- assets/base/_assetsData.php
+++ assets/base/_assetsData.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * P2AssetBundle.php
+ * _assetsData.php
  *
  * @author Pedro Plowman
- * @copyright Copyright &copy; Pedro Plowman, 2017
+ * @copyright Copyright &copy; Pedro Plowman, 2019
  * @link https://github.com/p2made
  * @license MIT
  *
@@ -11,60 +11,5 @@
  * @class \p2m\assets\base\P2AssetBundle
  */
 
-/**
- * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
- * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
- * ##### ^ #####                                           ##### ^ #####
- * ##### ^ #####      DO NOT USE THIS CLASS DIRECTLY!      ##### ^ #####
- * ##### ^ #####                                           ##### ^ #####
- * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
- * ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ ##### ^ #####
- */
-
-namespace p2m\assets\base;
-
-class P2AssetBundle extends \p2m\base\assets\P2AssetBase
-{
-	/*
-	 * @var string
-	 * protected $version;
-	 */
-
-	/*
-	 * @var string
-	 * protected $_p2mProjectId;
-	 */
-	protected $_p2mProjectId = 'yii2-p2y2-things';
-
-	/**
-	 * @var string
-	 * public $sourcePath;
-	 *
-	 * @var string
-	 * public $baseUrl;
-	 *
-	 * @var array
-	 * public $css = [];
-	 *
-	 * @var array
-	 * public $cssOptions = [];
-	 *
-	 * @var array
-	 * public $js = [];
-	 *
-	 * @var array
-	 * public $jsOptions = [];
-	 *
-	 * @var array
-	 * public $depends = [];
-	 *
-	 * @var array
-	 * public $publishOptions = [];
-	 *
-	 * @var boolean
-	 * public $_useStatic = false;
-	 *
-	 * @var array | false
-	 * public $_staticEnd = [] | false;
-	 */
-}
+return [
+];
