66fd84ee01e866ba96dc8fe7c5f50ee3721caa11
Pedro Plowman <pedrofp@me.com>
2019-09-03 18:18:29 +1000

    Update WowAsset.php

--- assets/WowAsset.php
+++ assets/WowAsset.php
@@ -25,33 +25,12 @@ namespace p2m\assets;
 
 class WowAsset extends \p2m\assets\base\P2AssetBundle
 {
-	protected $version = '1.1.2';
-
-	protected $resourceData = [
-		'static' => [
-			'baseUrl' => 'https://cdnjs.cloudflare.com/ajax/libs/wow/##-version-##',
-			'jsOptions' => [
-				'integrity' => 'sha256-z6FznuNG1jo9PP3/jBjL6P3tvLMtSwiVAowZPOgo56U=',
-				'crossorigin' => 'anonymous',
-			],
-		],
-		'published' => [
-			'sourcePath' => '@bower/wow/dist',
-		],
-	];
-
-	public $js = [
-		'wow.min.js'
-	];
-
-	public $depends = [
-		'p2m\assets\P2CoreAsset'
-		'p2m\assets\AnimateAsset',
-	];
+	protected $assetName = 'wow';
 
 	public function init()
 	{
-		$this->configureAsset($this->resourceData);
+		$this->assetData = $this->_assetData[$assetName];
+		$this->configureCdnjsAsset();
 		parent::init();
 	}
 }
