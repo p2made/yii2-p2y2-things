c677cfdd3aa479c4a77afaa6d4b284fddeca3b62
Pedro Plowman <pedrofp@me.com>
2019-09-04 12:55:33 +1000

    update data file

--- assets/base/_assetsData.php
+++ assets/base/_assetsData.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * _assetsData.php
- *
- * @author Pedro Plowman
- * @copyright Copyright &copy; Pedro Plowman, 2019
- * @link https://github.com/p2made
- * @license MIT
- *
- * @package p2made/yii2-p2y2-things
- */
-
 return [
 	'amplitudejs' => [ // Cdnjs
 		'pattern' => 'cdnjs',
