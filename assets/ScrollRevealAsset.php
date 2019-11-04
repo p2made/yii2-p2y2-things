22968562ec1a040ecb42e2449794b94b55cbcac5
Pedro Plowman <pedrofp@me.com>
2019-11-05 03:19:03 +1000

    Create ScrollRevealAsset.php

---
+++ assets/ScrollRevealAsset.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * ScrollRevealAsset.php
+ *
+ * Yii2 asset for ScrollReveal
+ * https://scrollrevealjs.org
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2019
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-moment
+ * @class \p2m\assets\ScrollRevealAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\ScrollRevealAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\ScrollRevealAsset',
+ */
+
+namespace p2m\assets;
+
+class ScrollRevealAsset extends \p2m\assets\base\P2AssetBundle
+{
+	protected $packageName = 'scrollreveal';
+
+	protected $packageVersion = '4.0.5';
+
+	protected $packageData = [
+		'static' => [
+			'baseUrl' => 'https://cdn.jsdelivr.net/npm/scrollreveal@##version##/dist',
+			'js' => [
+				'scrollreveal.min.js',
+			],
+			'jsOptions' => [
+				'integrity' => 'sha384-plDrqMHgu5akDTXIrAQhPO/4CW+KQgfTc1aXcxU9BFcm2RokcPRlqLWclkVN0tf4',
+				'crossorigin' => 'anonymous',
+			],
+		],
+		'published' => [
+			'sourcePath' => '@npm/scrollreveal/dist',
+			'js' => [
+				'scrollreveal.min.js',
+			],
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	];
+}
