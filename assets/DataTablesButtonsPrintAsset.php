a9c40530da98901e73890d91dacefeb123e4f405
Pedro Plowman <pedrofp@me.com>
2019-11-05 04:01:39 +1000

    Create DataTablesButtonsPrintAsset.php

---
+++ assets/DataTablesButtonsPrintAsset.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * DataTablesButtonsPrintAsset.php
+ *
+ * Yii2 asset for DataTables
+ * https://datatables.net
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2019
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-moment
+ * @class \p2m\assets\DataTablesButtonsPrintAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\DataTablesButtonsPrintAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\DataTablesButtonsPrintAsset',
+ */
+
+namespace p2m\assets;
+
+class DataTablesButtonsPrintAsset extends \p2m\assets\base\P2AssetBundle
+{
+	protected $packageName = 'datatables.net-buttons';
+
+	protected $packageVersion = '1.6.1';
+
+	protected $packageData = [
+		'static' => [
+			'baseUrl' => 'https://cdn.datatables.net/buttons/##version##',
+			'js' => [
+				'js/buttons.print.min.js',
+			],
+			'jsOptions' => [
+				'integrity' => 'sha384-XB54OBl3rOOjrhqlr+qwWqLv0GRnzVGFYKHPwdJnah4TyB7vQuYckQSsxQxGOA8d',
+				'crossorigin' => 'anonymous',
+			],
+		],
+		'published' => [
+			'sourcePath' => '@npm/datatables.net-buttons',
+			'js' => [
+				'js/buttons.print.min.js',
+			],
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+			'p2m\assets\DataTablesButtonsAsset',
+		],
+	];
+}
