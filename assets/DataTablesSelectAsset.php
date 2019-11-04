64e771970e6bd4094cfe6d82f202d18317ff9cce
Pedro Plowman <pedrofp@me.com>
2019-11-05 04:33:46 +1000

    Create DataTablesSelectAsset.php

---
+++ assets/DataTablesSelectAsset.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * DataTablesSelectAsset.php
+ *
+ * Yii2 asset for DataTables
+ * https://datatables.net
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2019
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-moment
+ * @class \p2m\assets\DataTablesSelectAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\DataTablesSelectAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\DataTablesSelectAsset',
+ */
+
+namespace p2m\assets;
+
+class DataTablesSelectAsset extends \p2m\assets\base\P2AssetBundle
+{
+	protected $packageName = 'datatables.net-select-bs4';
+
+	protected $packageVersion = '1.3.1';
+
+	protected $packageData = [
+		'static' => [
+			'baseUrl' => 'https://cdn.datatables.net/select/##version##',
+			'css' => [
+				'css/select.bootstrap4.min.css',
+			],
+			'cssOptions' => [
+				'integrity' => 'sha384-GT95DpievPCoEMQK7wFUKNSmBUItft1absIeFqP30FKpnjz8YPvV34CC79GirigE',
+				'crossorigin' => 'anonymous',
+			],
+			'js' => [
+				'js/dataTables.select.min.js',
+			],
+			'jsOptions' => [
+				'integrity' => 'sha384-VH47LLsycMvCVCOaoWxeGUxJ+e+vC+aJ1sqzh5hZRyOLF28zVh/sBrJGW4tyrhI5',
+				'crossorigin' => 'anonymous',
+			],
+		],
+		'published' => [
+			'sourcePath' => '@npm/datatables.net-select-bs4',
+			'css' => [
+				'css/select.bootstrap4.min.css',
+			],
+			'js' => [
+				'js/select.bootstrap4.min.js',
+			],
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+			'p2m\assets\DataTablesAsset',
+		],
+	];
+}
