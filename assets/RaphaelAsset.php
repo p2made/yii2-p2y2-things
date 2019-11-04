599dec737da927978aec0ab385c40907c3721775
Pedro Plowman <pedrofp@me.com>
2019-11-05 03:16:29 +1000

    Create RaphaelAsset.php

---
+++ assets/RaphaelAsset.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * RaphaelAsset.php
+ *
+ * Yii2 asset for Raphaël
+ * http://dmitrybaranovskiy.github.io/raphael/
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2019
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-moment
+ * @class \p2m\assets\RaphaelAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\RaphaelAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\RaphaelAsset',
+ */
+
+namespace p2m\assets;
+
+class RaphaelAsset extends \p2m\assets\base\P2AssetBundle
+{
+	protected $packageName = 'raphael';
+
+	protected $packageVersion = '2.3.0';
+
+	protected $packageData = [
+		'static' => [
+			'baseUrl' => 'https://cdn.jsdelivr.net/npm/raphael@##version##',
+			'js' => [
+				'raphael.min.js',
+			],
+			'jsOptions' => [
+				'integrity' => 'sha384-5fwEhxN8gjmQlK6ngccfyi8BVu5leqLqTqliCXNoLVGCjpGfzJwJ2pS8BLDIxWSt',
+				'crossorigin' => 'anonymous',
+			],
+		],
+		'published' => [
+			'sourcePath' => '@npm/raphael',
+			'js' => [
+				'raphael.min.js',
+			],
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	];
+}
