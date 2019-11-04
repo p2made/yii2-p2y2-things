c803cb4ff9544a6c2ce41d8e493c9bbe9509b969
Pedro Plowman <pedrofp@me.com>
2019-11-05 02:16:02 +1000

    Create ImagesLoadedAsset.php

---
+++ assets/ImagesLoadedAsset.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * ImagesLoadedAsset.php
+ *
+ * Yii2 asset for imagesLoaded
+ * https://imagesloaded.desandro.com
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright &copy; Pedro Plowman, 2019
+ * @link https://github.com/p2made
+ * @license MIT
+ *
+ * @package p2made/yii2-p2y2-moment
+ * @class \p2m\assets\ImagesLoadedAsset
+ */
+
+/**
+ * Load this asset with...
+ * p2m\assets\ImagesLoadedAsset::register($this);
+ *
+ * or specify as a dependency with...
+ *     'p2m\assets\ImagesLoadedAsset',
+ */
+
+namespace p2m\assets;
+
+class ImagesLoadedAsset extends \p2m\assets\base\P2AssetBundle
+{
+	protected $packageName = 'imagesloaded';
+
+	protected $packageVersion = '4.1.4';
+
+	protected $packageData = [
+		'static' => [
+			'baseUrl' => 'https://cdn.jsdelivr.net/npm/imagesloaded@##version##',
+			'js' => [
+				'imagesloaded.pkgd.min.js',
+			],
+			'jsOptions' => [
+				'integrity' => 'sha384-YnGSHPPWEUDKMHFPOVmNP7Xyfwx5G0CHet6IoNgiX6CbFZS8gCeIfEgB1MgPwjdI',
+				'crossorigin' => 'anonymous',
+			],
+		],
+		'published' => [
+			'sourcePath' => '@npm/imagesloaded',
+			'js' => [
+				'imagesloaded.pkgd.min.js',
+			],
+		],
+		'depends' => [
+			'p2m\assets\P2CoreAsset',
+		],
+	];
+}
