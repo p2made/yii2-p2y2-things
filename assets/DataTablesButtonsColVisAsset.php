b6e54ba3613b764a435c7a3e8ead8bfbb07a8fe9
Pedro Plowman <pedrofp@me.com>
2019-11-07 02:05:01 +1000

    Update DataTablesButtonsColVisAsset.php

--- assets/DataTablesButtonsColVisAsset.php
+++ assets/DataTablesButtonsColVisAsset.php
@@ -43,7 +43,7 @@ class DataTablesButtonsColVisAsset extends \p2m\assets\base\P2AssetBundle
 			'js/buttons.colVis.min.js',
 		],
 		'depends' => [
-			'p2m\assets\P2CoreAsset',
+			'p2m\assets\DataTablesButtonsAsset',
 		],
 	];
 
