7ca2e4d4373fb5bd59cdb9307dbfd46edb839a2a
Pedro Plowman <pedrofp@me.com>
2019-11-07 02:06:55 +1000

    Update DataTablesFixedHeaderAsset.php

--- assets/DataTablesFixedHeaderAsset.php
+++ assets/DataTablesFixedHeaderAsset.php
@@ -53,7 +53,7 @@ class DataTablesFixedHeaderAsset extends \p2m\assets\base\P2AssetBundle
 			'js/fixedHeader.bootstrap4.min.js',
 		],
 		'depends' => [
-			'p2m\assets\P2CoreAsset',
+			'p2m\assets\DataTablesAsset',
 		],
 	];
 
