6ba08cea3b4c52444b89d6aa094748da88eb291b
Pedro Plowman <pedrofp@me.com>
2019-11-07 02:03:07 +1000

    Update DataTablesResponsiveAssetBase.php

--- assets/base/DataTablesResponsiveAssetBase.php
+++ assets/base/DataTablesResponsiveAssetBase.php
@@ -43,7 +43,7 @@ class DataTablesResponsiveBase extends \p2m\assets\base\P2AssetBundle
 			'js/dataTables.responsive.min.js',
 		],
 		'depends' => [
-			'p2m\assets\P2CoreAsset',
+			'p2m\assets\DataTablesAsset',
 		],
 	];
 
